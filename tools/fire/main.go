8ee0fb610946ac0ce64c82308e9d3e8e454ca169
Erik Bosman <erik@minemu.org>
2014-11-22 05:36:43 +0100

    add fps setting to fire

--- tools/fire/main.go
+++ tools/fire/main.go
@@ -10,10 +10,12 @@ import (
 
 var gamma, brightness float64
 var ledOrder led.LedOrder
+var fps int
 
 func init() {
 	flag.Float64Var(&gamma, "gamma", 1, "used gamma correction setting")
 	flag.Float64Var(&brightness, "brightness", 1., "used brighness setting")
+	flag.IntVar(&fps, "fps", 80, "frames per second")
 	ledOrder = led.RGB
 	flag.Var(&ledOrder, "ledorder", "led order")
 }
@@ -26,7 +28,10 @@ func main() {
 	out := drivers.LedDriver()
 	animation := fire.NewFire(model.LedballSmooth())
 
+	t := time.Tick(time.Second / fps)
+
 	for {
+		<-t
 		out.Write(strip.LoadFrame(animation.Next()))
 	}
 }

3fa32664652a8a8f6bb1bbab9290f81ee0f3b4e4
Erik Bosman <erik@minemu.org>
2015-04-09 02:51:19 +0200

    add json exporter for models

---
+++ tools/model/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+    "fmt"
+	"os"
+	"post6.net/goled/model"
+	"post6.net/goled/model/polyhedrone"
+    "io"
+)
+
+
+func writeLed(out io.Writer, led model.Led3D) {
+	boolStr := map[bool]string{ true: "true", false: "false" }
+	fmt.Fprintf(out, "\t\t{\n"+
+	                 "\t\t\t\"position\": [%f, %f, %f],\n"+
+	                 "\t\t\t\"normal\": [%f, %f, %f],\n"+
+	                 "\t\t\t\"inside\": %s\n"+
+	                 "\t\t}",
+	                 led.Position.X, led.Position.Y, led.Position.Z,
+	                 led.Normal.X, led.Normal.Y, led.Normal.Z,
+	                 boolStr[led.Inside])
+}
+
+func writeModel(out io.Writer, leds []model.Led3D) {
+
+	fmt.Fprintf(out, "{\n\t\"leds\": [\n")
+	for i,led := range leds {
+		writeLed(out, led)
+		if i < len(leds)-1 {
+			fmt.Fprintf(out, ",\n")
+		} else {
+			fmt.Fprintf(out, "\n")
+		}
+	}
+	fmt.Fprintf(out, "\t]\n}\n")
+}
+
+func main() {
+
+	writeModel(os.Stdout, polyhedrone.Ledball().Leds)
+}

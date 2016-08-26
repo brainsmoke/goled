89630aa41daf7e0c268c647df643651c75b9109e
Erik Bosman <erik@minemu.org>
2016-08-26 22:06:25 +0200

    shadowplay: use polyhedron info to determine inside/outside

--- ani/shadowplay/shadowplay.go
+++ ani/shadowplay/shadowplay.go
@@ -12,6 +12,7 @@ type ShadowPlay struct {
 	phaseMax, phase int
 	colors          [][3]byte
 	indices         []int
+	insideLeds      []int
 	wave            []int
 	buf             [][3]byte
 }
@@ -23,12 +24,19 @@ func NewShadowPlay(leds []model.Led3D, phaseMax, nColors int) *ShadowPlay {
 	wave := make([]int, phaseMax)
 	colors := make([][3]byte, nColors)
 	indices := make([]int, nColors)
+	insideLeds := []int{}
 
 	for i := range wave {
 		wave[i] = int(127 - math.Cos(math.Pi*2*float64(i)/float64(phaseMax))*127)
 	}
 
-	return &ShadowPlay{phaseMax: phaseMax, phase: 0, buf: buf, wave: wave, colors: colors, indices: indices}
+	for i := range leds {
+		if leds[i].Inside {
+			insideLeds = append(insideLeds, i)
+		}
+	}
+
+	return &ShadowPlay{phaseMax: phaseMax, phase: 0, buf: buf, wave: wave, colors: colors, indices: indices, insideLeds: insideLeds}
 }
 
 func (t *ShadowPlay) Next() [][3]byte {
@@ -40,7 +48,7 @@ func (t *ShadowPlay) Next() [][3]byte {
 	for i := range t.colors {
 		p := (t.phase + i*t.phaseMax/len(t.colors)) % t.phaseMax
 		if p == 0 {
-			t.indices[i] = rand.Intn(len(t.buf)/5)*5 + 4
+			t.indices[i] = t.insideLeds[rand.Intn(len(t.insideLeds))]
 			//t.colors[i] = [3]byte{byte(rand.Intn(256)), byte(rand.Intn(256)), byte(rand.Intn(256))}
 			t.colors[i] = color.HSIToRGB(rand.Float64(), 1, rand.Float64())
 

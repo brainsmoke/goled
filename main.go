d5a595da751b287f9e23ccd458d25d415becc923
Erik Bosman <erik@minemu.org>
2016-08-26 22:10:15 +0200

    add support for ws2811 based model

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"post6.net/goled/ani/cache"
 	"post6.net/goled/ani/fire"
 	"post6.net/goled/ani/five"
+	"post6.net/goled/ani/fifteen"
 	"post6.net/goled/ani/gameoflife"
 	"post6.net/goled/ani/gradient"
 	"post6.net/goled/ani/image"
@@ -27,6 +28,7 @@ import (
 	"post6.net/goled/model"
 	"post6.net/goled/model/poly/minipoly"
 	"post6.net/goled/model/poly/polyhedrone"
+	"post6.net/goled/model/poly/poly12"
 	"time"
 )
 
@@ -67,7 +69,7 @@ var gamma, brightness float64
 var fps, switchTime, blendTime int
 var ledOrder led.LedOrder
 //var ambient bool
-var mini bool
+var mini, p12 bool
 var animations = []ani.Animation(nil)
 
 func addAni(a ani.Animation) {
@@ -81,6 +83,7 @@ func init() {
 	flag.IntVar(&switchTime, "switchtime", 60, "seconds per animation")
 //	flag.BoolVar(&ambient, "ambient", false, "don't load bright animations")
 	flag.BoolVar(&mini, "mini", false, "use small polyhedron model")
+	flag.BoolVar(&p12, "poly12", false, "use new polyhedron model")
 	ledOrder = led.RGB
 	flag.Var(&ledOrder, "ledorder", "led order")
 }
@@ -104,7 +107,10 @@ func main() {
 	var ball *model.Model3D
 	var inside bool
 
-	if mini {
+	if p12 {
+		ball = poly12.Ledball()
+		inside = true
+	} else if mini {
 		ball = minipoly.Ledball()
 		inside = false
 	} else {
@@ -112,6 +118,7 @@ func main() {
 		inside = true
 	}
 	unitBall := ball.UnitScale()
+	//smoothUnitBall := ball.UnitScale().Smooth()
 	smooth := ball.Smooth()
 
 	strip := led.NewLedStrip(len(ball.Leds), ledOrder, gamma, brightness)
@@ -129,6 +136,7 @@ func main() {
 
 	baseDir := path.Dir(os.Args[0])
 	earth, _ := os.Open(baseDir + "/earth.png")
+	newImg, _ := os.Open(baseDir + "/newimg.png")
 
 	if inside {
 		addAni(wobble.NewWobble(smooth.Leds, wobble.Inside))
@@ -137,18 +145,39 @@ func main() {
 	addAni(wobble.NewWobble(unitBall.Leds, wobble.Outside))
 	addAni(snake.NewSnake(ball))
 	addAni(cache.NewCachedAni(image.NewImageAni(smooth.Leds, earth, 0, 0, 0), len(smooth.Leds), 256))
+	addAni(cache.NewCachedAni(image.NewImageAni(smooth.Leds, newImg, 0, 0, 0), len(smooth.Leds), 256))
 	if inside {
 		addAni(shadowwalk.NewShadowWalk(smooth.Leds))
 	}
 	addAni(shadowplay.NewShadowPlay(ball.Leds, 512, 8))
+	addAni(shadowplay.NewShadowPlay(ball.Leds, 512, 32))
 	addAni(topo.NewTopo(ball))
 	addAni(orbit.NewOrbitAni(unitBall.Leds))
-	addAni(gradient.NewGradient(smooth.Leds, gradient.Hard))
+	//addAni(orbit.NewOrbitAni(smoothUnitBall.Leds))
 	addAni(gameoflife.NewGameOfLife(ball))
-	addAni(gradient.NewGradient(smooth.Leds, gradient.Smooth))
-	addAni(five.NewFive(ball.Leds))
+	addAni(gradient.NewGradient(smooth.Leds, gradient.Hard, gradient.Outside))
+	addAni(gradient.NewGradient(smooth.Leds, gradient.Hard, gradient.Inside))
+	addAni(gradient.NewGradient(smooth.Leds, gradient.Binary, gradient.Outside))
+	addAni(gradient.NewGradient(smooth.Leds, gradient.Smooth, gradient.Outside))
+	addAni(gradient.NewGradient(smooth.Leds, gradient.Striped, gradient.Outside))
+	addAni(gradient.NewGradient(smooth.Leds, gradient.Striped, gradient.Inside))
+
+	addAni(gradient.NewSpiral(smooth.Leds, gradient.Hard, gradient.Outside))
+	addAni(gradient.NewSpiral(smooth.Leds, gradient.Binary, gradient.Outside))
+	addAni(gradient.NewSpiral(smooth.Leds, gradient.Smooth, gradient.Outside))
+	addAni(gradient.NewSpiral(smooth.Leds, gradient.Striped, gradient.Outside))
+	addAni(gradient.NewSpiral(smooth.Leds, gradient.Striped, gradient.Inside))
+	if p12 {
+		addAni(fifteen.NewFifteen(ball.Leds))
+	} else {
+		addAni(five.NewFive(ball.Leds))
+	}
 	addAni(radar.NewRadar(smooth.Leds))
-	addAni(five.NewFiveWave(smooth.Leds))
+	if p12 {
+		addAni(fifteen.NewFifteenWave(smooth.Leds))
+	} else {
+		addAni(five.NewFiveWave(smooth.Leds))
+	}
 
 	current, last := 0, -1
 	blendIter := 0
@@ -156,7 +185,7 @@ func main() {
 
 	var curFrame, lastFrame, tmpFrame [][3]byte
 
-	tmpFrame = make([][3]byte, 300)
+	tmpFrame = make([][3]byte, len(ball.Leds))
 
 	for {
 		select {
